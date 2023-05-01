b2072963152ee047eaacb983664027b1d968e7c8
César Guerra <warderer@gmail.com>
2023-05-01 13:54:39 -0600

    ✨ useTranslator

---
+++ hooks/useTranslator.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import debounce from '@/utils/debounce'
+
+const HALF_SECOND_IN_MS = 500;
+
+const useTranslator = () => {
+    const [inputText, setInputText] = useState<string>('');
+    const [translatedText, setTranslatedText] = useState<string>('');
+
+    // Manejador del estado del text area
+    const handleInput = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setInputText(event.target.value);
+    }
+
+    const translateText = async (text: string) => {
+        const response = await fetch('api/translate', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text })
+        });
+
+        if(response.ok) {
+            const data = await response.json();
+            setTranslatedText(data.translatedText);
+        }
+    }
+
+    const debouncedTranslateText = debounce(translateText, HALF_SECOND_IN_MS);
+
+    useEffect( () => {
+        if (inputText) {
+            debouncedTranslateText(inputText)
+        }
+    }, [inputText, debouncedTranslateText])
+
+    return { input: inputText, translateText, handleInput};
+}
